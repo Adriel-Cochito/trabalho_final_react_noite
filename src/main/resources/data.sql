dfc42c99f206e66b71f3d9071206fbcd3b4b7a4d
Adriel Cochito <adrielcochito.dev@gmail.com>
2025-01-04 13:50:36 -0300

    Update data.sql
    Usuario Adriel

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -70,6 +70,7 @@ INSERT INTO carro (modelo, ano, cor, cavalos_de_potencia, fabricante, pais, stat
 
 
 INSERT INTO usuario (nome, cargo, email, avatar, password) VALUES
+                                                               ('Adriel H B Cochito', 'Engenheiro de Software', 'adriel@acme.com', 'https://www.gravatar.com/avatar/89adf1a694e928f9c6da1c1f9b998d1a?s=200', '123123'),
                                                                ('Wile E. Coyote', 'Engenheiro de Produtos', 'wile@acme.com', 'https://www.gravatar.com/avatar/89adf1a694e928f9c6da1c1f9b998d1a?s=200', 'senha123'),
                                                                ('Road Runner', 'Testador de Produtos', 'roadrunner@acme.com', 'https://www.gravatar.com/avatar/af5a6b8ed7b0a37e25b1cf5e9e22a8f2?s=200', 'senha123'),
                                                                ('Daffy Duck', 'Gerente de Marketing', 'daffy@acme.com', 'https://www.gravatar.com/avatar/daf4b5b81342fe587d5043b3e58a0180?s=200', 'senha123'),
